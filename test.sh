bec9e90a55678b268eb97d0aab12717d4422f79c
d-maasland <donny@eset.nl>
2017-12-05 16:30:13 +0100

    test

--- test.sh
+++ test.sh
@@ -2,6 +2,3 @@
 
 eav_user="$1"
 eav_pass="$2"
-esets_url="https://download.eset.com/com/eset/apps/business/es/linux/latest/esets.amd64.deb.bin"
-
-wget --user="$eav_user" --password="$eav_pass" "$esest_url"
