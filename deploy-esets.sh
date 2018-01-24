0a38f1555242a635cf73b0ffa64fd5693e6f8f5e
esetnl <35723681+esetnl@users.noreply.github.com>
2018-01-24 09:04:34 +0100

    Update deploy-esets.sh

--- deploy-esets.sh
+++ deploy-esets.sh
@@ -292,6 +292,8 @@ download_agentscript() {
     if [ "$?" != "0" ]; then
       exit 19
     fi
+    
+    /bin/chmod +x -- "/home/$adminusername/install_eraagent.sh"
 }
 
 configure_nginx() {
@@ -332,9 +334,6 @@ server {
 
   error_page 400 401 402 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 418 420 422 423 424 426 428 429 431 444 449 450 451 500 501 502 503 504 505 506 507 508 509 510 511 /error.html;
 
-  location /error.html {
-    root /usr/share/nginx/html;
-  }
 
 }
 EOF
